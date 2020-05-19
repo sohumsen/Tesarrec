63b75e35d0ace2e63540e51fddf5d45c08511f05
sohumsen <sohumsen@gmail.com>
2020-05-19 15:07:33 +0100

    MFC Update

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,24 +1,16 @@
-import React, {Component} from 'react'
-import classes from './Layout.module.css'
-import Toolbar from '../../components/Navigation/Toolbar/Toolbar'
-import LeftContent from './LeftContent/LeftContent'
-class Layout extends Component{
-
-    render(){
-
-        
-        return(
-            <div>
-                <Toolbar/>
-                <LeftContent/>
-                <main className={classes.centerContent}>
-                    {this.props.children}
-                </main>
-                
-            </div>
-
-        )
-    }
-
+import React, { Component } from "react";
+import classes from "./Layout.module.css";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import LeftContent from "./LeftContent/LeftContent";
+class Layout extends Component {
+  render() {
+    return (
+      <div>
+        <Toolbar />
+        <LeftContent />
+        <main className={classes.centerContent}>{this.props.children}</main>
+      </div>
+    );
+  }
 }
-export default Layout
+export default Layout;
